fd3366fe2ad3ef074e2a80efdf1c99086dd5c934
Irakli <i.dzigrashvili@gmail.com>
2018-10-07 14:44:23 +0400

    Commit 5

--- src/app.js
+++ src/app.js
@@ -4,10 +4,19 @@ import mongoose, { mongo } from 'mongoose';
 import {router} from './config/routes';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/recipeDb4');
+//mongoose.connect('mongodb://localhost/recipeDb4');
+mongoose.connect("mongodb+srv://Irakli:mO8kaSItCYmwOMGr@cluster0-rwizn.mongodb.net/test?retryWrites=true")
+    .then (()=> {
+        console.log('conencted to Mongo Atlas')
+})
+.catch(()=> {
+    console.log('Connection Faield to Mongo Atlas');
+})
+
 
 const app = express();
 const PORT = 3001;
+//mO8kaSItCYmwOMGr
 
 app.use(express.json());
 app.use(express.urlencoded());
